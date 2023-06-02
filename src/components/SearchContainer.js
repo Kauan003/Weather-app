ccbfe80f1b78fb6d8039a1af52d63761f27b7ca1
Kauan <kauan346goncalves@gmail.com>
2023-06-02 00:28:13 -0300

    add images for current weather

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,55 +1,67 @@
 import React, { useState } from 'react';
 import styles from './SearchContainer.module.css';
-import { RiSunLine, RiCloudyLine, RiRainyLine, RiSnowyLine, RiThunderstormsLine  } from 'react-icons/ri';
+// import { RiSunLine, RiCloudyLine, RiRainyLine, RiSnowyLine, RiThunderstormsLine, RiMoonClearLine   } from 'react-icons/ri';
 
 function SearchContainer() {
-  const [inputValue, setInputValue] = useState('');
-  const [weatherData, setWeatherData] = useState(null); // Novo estado para armazenar os dados
+  const [inputValue, setInputValue] = useState('')
+  const [weatherData, setWeatherData] = useState(null) // Novo estado para armazenar os dados
 
   const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+    setInputValue(event.target.value)
   };
 
-  const apiKey = "23cf8ee0be25bf809bc65e16be73041b";
-  const inputVal = inputValue.toLowerCase();
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  const apiKey = "23cf8ee0be25bf809bc65e16be73041b"
+  const inputVal = inputValue.toLowerCase()
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`
 
   const handleFormSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        const { main, name, sys, weather } = data;
+        const { main, name, sys, weather } = data
         const temperature = Math.round(main.temp)
-        setWeatherData({ main, name, sys, weather, temperature}); // Atualiza o estado com os dados recebidos
+        setWeatherData({ main, name, sys, weather, temperature}) // Atualiza o estado com os dados recebidos
       })
       .catch(() => {
-        console.log('Ocorreu um erro');
-      });
-  };
-
-  let weatherIcon;
-
-  if (weatherData && weatherData.weather[0]) {
-    const weatherCode = weatherData.weather[0].id;
-
-    if (weatherCode >= 200 && weatherCode <= 232) {
-      weatherIcon = <RiThunderstormsLine />;
-    } else if (weatherCode >= 300 && weatherCode <= 531) {
-      weatherIcon = <RiRainyLine />;
-    } else if (weatherCode >= 600 && weatherCode <= 622) {
-      weatherIcon = <RiSnowyLine />;
-    } else if (weatherCode >= 701 && weatherCode <= 781) {
-      weatherIcon = <RiCloudyLine />;
-    } else if (weatherCode === 800) {
-      weatherIcon = <RiSunLine />;
-    } else {
-      weatherIcon = null;
-    }
+        console.log('Ocorreu um erro')
+      })
   }
 
+  const weatherBackgrounds = {
+    thunderstorms: 'url(src/images/thunderstorms-image.webp)',
+    rainy: 'url(src/images/rainy-image.webp)',
+    snowy: 'url(src/images/snowy-image.webp)',
+    cloudy: 'url(src/images/cloudy-image.webp)',
+    sunny: 'url(src/images/sunny-image.webp)',
+  }
+
+  let backgroundImage = null;
+
+if (weatherData && weatherData.weather[0]) {
+  const weatherCode = weatherData.weather[0].id;
+
+  if (weatherCode >= 200 && weatherCode <= 232) {
+    backgroundImage = weatherBackgrounds.thunderstorms;
+  } else if (weatherCode >= 300 && weatherCode <= 531) {
+    backgroundImage = weatherBackgrounds.rainy;
+  } else if (weatherCode >= 600 && weatherCode <= 622) {
+    backgroundImage = weatherBackgrounds.snowy;
+  } else if (weatherCode >= 701 && weatherCode <= 781) {
+    backgroundImage = weatherBackgrounds.cloudy;
+  } else if (weatherCode === 800) {
+    backgroundImage = weatherBackgrounds.sunny;
+  }
+}
+  let weatherIconUrl = null;
+
+if (weatherData && weatherData.weather[0]) {
+  const iconCode = weatherData.weather[0].icon;
+  weatherIconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${iconCode}.svg`;
+}
+
   return (
-    <div className={styles.box}>
+    <div className={`${styles.box} ${backgroundImage && styles.withBackground}`} style={{ backgroundImage }}>
       <div className={styles.search}>
         <form onSubmit={handleFormSubmit}>
           <input type="text" value={inputValue} onChange={handleInputChange} placeholder="City" />
@@ -58,10 +70,10 @@ function SearchContainer() {
       </div>
       {weatherData && (
       <div>
-        {/* Renderize o ícone */}
-        {weatherIcon}
+        {/* Renderize a imagem do ícone */}
+        {weatherIconUrl && <img src={weatherIconUrl} alt="Weather Icon" />}
         <p>City: {weatherData.name}</p>
-        <p>Temperature: {weatherData.temperature}°C</p>
+        <p>Temperature: {weatherData.main.temp}°C</p>
         <p>Description: {weatherData.weather[0].description}</p>
       </div>
     )}
